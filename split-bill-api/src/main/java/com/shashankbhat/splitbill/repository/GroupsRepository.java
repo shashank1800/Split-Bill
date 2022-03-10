939a21b3fb0a555869d5bc8f963c1ea7dbf0caa1
shashank1800 <shashankbhat1800@gmail.com>
2022-03-10 21:16:05 +0530

    group sql

--- split-bill-api/src/main/java/com/shashankbhat/splitbill/repository/GroupsRepository.java
+++ split-bill-api/src/main/java/com/shashankbhat/splitbill/repository/GroupsRepository.java
@@ -1,6 +1,7 @@
 package com.shashankbhat.splitbill.repository;
 
 import com.shashankbhat.splitbill.entity.GroupsEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
@@ -9,10 +10,10 @@ import java.util.List;
 
 @Component
 public interface GroupsRepository extends JpaRepository<GroupsEntity, Integer> {
-
-    @Query(value = "SELECT * FROM groups_tbl WHERE unique_id = ?1 " +
-            "UNION " +
-            "SELECT * FROM groups_tbl g WHERE g.id IN (SELECT DISTINCT u.group_id FROM users u WHERE unique_id = ?1) " +
-            "ORDER BY date_created DESC;", nativeQuery = true)
-    List<GroupsEntity> findAllGroupsWithUniqueId(Integer uniqueId);
+    List<GroupsEntity> findByUniqueId(Integer uniqueId/*, Sort sort*/);
+//    @Query(value = "SELECT * FROM groups_tbl WHERE unique_id = ?1 " +
+//            "UNION " +
+//            "SELECT * FROM groups_tbl g WHERE g.id IN (SELECT DISTINCT u.group_id FROM users u WHERE unique_id = ?1) " +
+//            "ORDER BY date_created DESC;", nativeQuery = true)
+//    List<GroupsEntity> findAllGroupsWithUniqueId(Integer uniqueId);
 }
